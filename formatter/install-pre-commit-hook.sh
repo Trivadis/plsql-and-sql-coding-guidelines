c40a39c583bbaa92afcf895e267f5e92cc637840
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2021-10-11 00:33:22 +0200

    add formatter settings
    according https://github.com/Trivadis/plsql-formatter-settings/tree/main/hook#optimal-tvdformatjar-variant

---
+++ formatter/install-pre-commit-hook.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+FORMATTER_DIR="$(dirname $0)"
+GIT_HOOK_DIR="$FORMATTER_DIR/../.git/hooks"
+cp $FORMATTER_DIR/pre-commit $GIT_HOOK_DIR/pre-commit
+chmod +x $GIT_HOOK_DIR/pre-commit
+curl -o $GIT_HOOK_DIR/tvdformat.jar -L https://github.com/Trivadis/plsql-formatter-settings/releases/download/sqldev-21.2.1/tvdformat.jar
+echo "pre-commit hook installed in $GIT_HOOK_DIR/pre-commit."
