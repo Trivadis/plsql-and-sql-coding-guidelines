583683fdec0a56e93ced81550e567314ff42409b
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-07-08 08:51:02 +0200

    update formatter to sqldev-22.2.0

--- formatter/install-pre-commit-hook.sh
+++ formatter/install-pre-commit-hook.sh
@@ -4,5 +4,5 @@ FORMATTER_DIR="$(dirname $0)"
 GIT_HOOK_DIR="$FORMATTER_DIR/../.git/hooks"
 cp $FORMATTER_DIR/pre-commit $GIT_HOOK_DIR/pre-commit
 chmod +x $GIT_HOOK_DIR/pre-commit
-curl -o $GIT_HOOK_DIR/tvdformat.jar -L https://github.com/Trivadis/plsql-formatter-settings/releases/download/sqldev-21.4.3/tvdformat.jar
+curl -o $GIT_HOOK_DIR/tvdformat.jar -L https://github.com/Trivadis/plsql-formatter-settings/releases/download/sqldev-22.2.0/tvdformat.jar
 echo "pre-commit hook installed in $GIT_HOOK_DIR/pre-commit."
