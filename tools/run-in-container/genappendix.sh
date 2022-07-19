c404a2de858eb2552df7f2f90ef9d5d54bd57590
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-07-19 11:46:36 +0200

    sort append by file name (guideline number)

--- tools/run-in-container/genappendix.sh
+++ tools/run-in-container/genappendix.sh
@@ -10,4 +10,4 @@ echo "" >> $OUTFILE
 cat docs/_appendixdata/appendix_a.md.template >> $OUTFILE
 cat docs/_appendixdata/appendix_b.md.template >> $OUTFILE
 
-find docs/4-language-usage/ -name g-*.md | xargs head -n 4 -q | tools/run-in-container/genappendix_b_table.awk >> $OUTFILE
+find docs/4-language-usage/ -name g-*.md | sort | xargs head -n 4 -q | tools/run-in-container/genappendix_b_table.awk >> $OUTFILE
